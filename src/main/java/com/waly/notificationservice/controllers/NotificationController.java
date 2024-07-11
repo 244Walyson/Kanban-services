dadd8bef4ac14c105ba534eff40d4812c7aa9434
walyson moises <walysonmoises1342@gmail.com>
2024-07-11 02:22:45 -0300

    service discovery

--- src/main/java/com/waly/notificationservice/controllers/NotificationController.java
+++ src/main/java/com/waly/notificationservice/controllers/NotificationController.java
@@ -4,7 +4,6 @@ import com.waly.notificationservice.dtos.NotificationDTO;
 import com.waly.notificationservice.service.NotificationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
